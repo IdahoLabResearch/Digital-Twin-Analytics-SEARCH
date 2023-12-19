57aff11ae6967b51be80da2af591aeec97d8cfad
Katherine Wilsdon <katherine.wilsdon@inl.gov>
2023-12-19 10:09:29 -0700

    Minor Changes

--- install_dependencies_rbased.R
+++ install_dependencies_rbased.R
@@ -5,6 +5,6 @@ util_packages = c("devtools", "testthat", "xml2", "roxygen2", "reticulate",
                   "DT", "shinythemes", "shiny", "plotly", "ggplot2", "ggdendro")
 install.packages(util_packages, repos = r_repo)
 
-devtools::install_git("https://github.inl.gov/Digital-Engineering/SEARCH",
+devtools::install_git("https://github.com/IdahoLabResearch/Digital-Twin-Analytics-SEARCH",
                       dependencies = T, repos = r_repo)
 search::genPythonEnv()
